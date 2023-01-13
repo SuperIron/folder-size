71ce78ded20b0df713d61bb09996d770f3c6353c
ntla123 <58218596@qq.com>
2023-01-13 21:36:29 +0800

    sort

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import os
 import prettytable as pt
 from PyQt5 import QtWidgets
+import sys
 
 
 def get_directory():
+    app = QtWidgets.QApplication([])
     dialog = QtWidgets.QFileDialog(None, caption='Folder Size Dir')
     dialog.setOption(QtWidgets.QFileDialog.DontUseNativeDialog, True)
     dialog.setDirectory('./')
@@ -33,8 +35,12 @@ def get_tb(title):
 
 
 def init():
-    path = get_directory()
+    if len(sys.argv) >= 2:
+        path = sys.argv[1]
+    else:
+        path = get_directory()
     tb = get_tb(path)
+    logs = []
     child_dirs = os.listdir(path)
     for child_dir in child_dirs:
         child_dir_path = os.path.join(path, child_dir)
@@ -45,14 +51,18 @@ def init():
             for filename in filenames:
                 file_path = os.path.join(dirpath, filename)
                 sizes += os.path.getsize(file_path)
-        sizes, unit = get_unit(sizes)
-        tb.add_row(["%s" % (child_dir), "%.1f%s" % (sizes, unit)])
-
+        new_sizes, unit = get_unit(sizes)
+        logs.append({
+            'sizes': sizes,
+            'row': ["%s" % (child_dir), "%.1f%s" % (new_sizes, unit)]
+        })
+    logs = sorted(logs, key=lambda item: item['sizes'])
+    for log in logs:
+        tb.add_row(log['row'])
     print('Folder-Size v1.0.0', end='\n\n')
     print(tb, end='\n\n')
     os.system("pause")
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
     init()
